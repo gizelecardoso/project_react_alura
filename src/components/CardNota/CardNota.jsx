ddd5fbf11f49c72269db2e0dd9c89107d49d53bd
Gizele Santos <gizele.santos@repassa.com.br>
2021-01-29 17:04:09 -0300

    Finalizando primeira aplicaçao - e primeiro curso de React

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -7,9 +7,9 @@ class CardNota extends Component {
     return (  
       <section className="card-nota">
         <header className="card-nota_cabeçalho">
-            <h3 className="card-nota_titulo">Título</h3>
+            <h3 className="card-nota_titulo">{this.props.titulo}</h3>
         </header>
-        <p className="card-nota_texto">Escreva sua nota</p>
+        <p className="card-nota_texto">{this.props.texto}</p>
       </section>
     );
   }
